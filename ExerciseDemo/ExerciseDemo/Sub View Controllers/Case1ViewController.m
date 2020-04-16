e46125fb21cbf739f3811861a65ad7c232548b09
guqi <guqi@fenbi.com>
2020-04-16 11:45:35 +0800

    modified:   ExerciseDemo/ExerciseDemo/Sub View Controllers/Case1ViewController.m

--- ExerciseDemo/ExerciseDemo/Sub View Controllers/Case1ViewController.m
+++ ExerciseDemo/ExerciseDemo/Sub View Controllers/Case1ViewController.m
@@ -12,7 +12,6 @@
 
 @interface Case1ViewController () <UIScrollViewDelegate>
 
-@property (nonatomic, strong) UIView *containerView;
 @property (nonatomic, strong) UIImageView *imageView;
 @property (nonatomic, strong) UIScrollView *scrollView;
 
@@ -29,32 +28,13 @@
 
 - (void) initSubviews
 {
-//    [self.view addSubview:self.scrollView];
-//    [self.scrollView addSubview:self.containerView];
-//    [self.containerView addSubview:self.imageView];
-//
-//    [self.scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.edges.equalTo(self.view).with.insets
-//        (UIEdgeInsetsMake(64,0,self.view.bounds.size.height/3*2-50,0));
-//    }];
-//    [self.containerView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.edges.equalTo(self.scrollView);
-//        make.width.height.equalTo(self.scrollView);
-//    }];
-//    [self.imageView mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.left.top.right.equalTo(self.containerView);
-//        make.height.equalTo(@60);
-//    }];
-
     [self.view addSubview:self.scrollView];
     [self.scrollView addSubview:self.imageView];
 
     [self.scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.and.top.equalTo(@0);
+        make.edges.equalTo(@0);
     }];
     [self.imageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.and.bottom.equalTo(self.scrollView);
-        make.width.height.equalTo(self.scrollView);
     }];
 }
 
@@ -65,21 +45,12 @@
 
 #pragma mark - Getters & Setters
 
-- (UIView *)containerView
-{
-    if (!_containerView) {
-        _containerView = [[UIView alloc] init];
-        _containerView.backgroundColor = [UIColor redColor];
-    }
-    return _containerView;
-}
-
 - (UIImageView *)imageView
 {
     if (!_imageView) {
         _imageView = [[UIImageView alloc] init];
-        [_imageView sd_setImageWithURL:[NSURL URLWithString:@"https://i0.hdslb.com/bfs/article/d1f0c4613eca28e934185bdd5a36cbce24606bbc.jpg"]];
-        _imageView.backgroundColor = [UIColor grayColor];
+//        [_imageView sd_setImageWithURL:[NSURL URLWithString:@"https://i0.hdslb.com/bfs/article/d1f0c4613eca28e934185bdd5a36cbce24606bbc.jpg"]];
+        _imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"https://i0.hdslb.com/bfs/article/d1f0c4613eca28e934185bdd5a36cbce24606bbc.jpg"]]];
     }
     return _imageView;
 }
@@ -92,7 +63,7 @@
         _scrollView.minimumZoomScale = 0.2;
         _scrollView.maximumZoomScale = 3.0;
         _scrollView.delegate = self;
-        self.scrollView.contentSize = self.imageView.image ? self.self.imageView.image.size : CGSizeZero;
+        _scrollView.contentSize = self.imageView.image ? self.imageView.image.size : CGSizeZero;
     }
     return _scrollView;
 }
